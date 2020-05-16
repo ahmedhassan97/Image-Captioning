dee5cf1e013495b60cd59bdc09b2dfe137e95974
Ahmed Khaled <mysterious588@users.noreply.github.com>
2020-05-16 23:44:16 +0200

    initial commit

---
+++ model.py
@@ -0,0 +1,86 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+
+class EncoderCNN(nn.Module):
+    def __init__(self, embed_size):
+        super(EncoderCNN, self).__init__()
+        resnet = models.resnet50(pretrained=True)
+        for param in resnet.parameters():
+            param.requires_grad_(False)
+        
+        modules = list(resnet.children())[:-1]
+        self.resnet = nn.Sequential(*modules)
+        self.embed = nn.Linear(resnet.fc.in_features, embed_size)
+
+    def forward(self, images):
+        features = self.resnet(images)
+        features = features.view(features.size(0), -1)
+        features = self.embed(features)
+        return features
+    
+
+class DecoderRNN(nn.Module):
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
+        super(DecoderRNN, self).__init__()
+        
+        self.embed_size = embed_size
+        self.hidden_size = hidden_size
+        self.vocab_size = vocab_size
+        self.num_layers = num_layers
+        
+        self.embedding = nn.Embedding(vocab_size, embed_size)
+        
+        self.lstm = nn.LSTM(input_size=embed_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True)
+        
+        self.fc = nn.Linear(hidden_size, vocab_size)
+        
+        self.dropout = nn.Dropout(0.25)
+    
+    def forward(self, features, captions):
+        
+        #print('shape', captions.shape)
+        
+        captions = captions[:, :-1]
+        
+        #print('shape', captions.shape)
+
+        captions = self.embedding(captions)
+        
+        #print('shape', features.shape)
+
+        features = features.unsqueeze(1)
+        #print('shape', features.shape)
+        
+        output, hidden = self.lstm(torch.cat((features, captions), dim=1))
+        
+        output = self.dropout(output)
+        output = self.fc(output)
+        
+        return output
+
+    def sample(self, inputs, states=None, max_len=20):
+        
+        sentence = []
+        i = 0
+        word_item = None
+        
+        while i < max_len:            
+            # reached the <end> word
+            if word_item == 1:
+                break
+            
+            output, states = self.lstm(inputs, states)
+            output = self.fc(output)
+            
+            _, word = output.max(2)
+            
+            word_item = word.item()
+            sentence.append(word_item)
+            
+            inputs = self.embedding(word)
+            
+            i+=1
+        
+        return sentence
